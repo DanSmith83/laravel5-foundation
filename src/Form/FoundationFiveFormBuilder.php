182a9fc211b8cbff25ce1ec4e0235c7b7f904912
Daniel Smith <dan.smith@openstudycollege.com>
2015-02-06 13:32:22 +0000

    Add label methods

--- src/Form/FoundationFiveFormBuilder.php
+++ src/Form/FoundationFiveFormBuilder.php
@@ -16,6 +16,32 @@ class FoundationFiveFormBuilder extends FormBuilder {
      */
     public function foundationText($name, $value = null, $options = array())
     {
-        return '<label>'.$this->input('text', $name, $value, $options).'</label>';
+        return $this->startLabel($name).
+               $this->input('text', $name, $value, $options).
+               $this->endLabel();
+    }
+
+    /**
+     * Create a form label element.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function startLabel($name, $value = null, $options = array())
+    {
+        $this->labels[] = $name;
+
+        $options = $this->html->attributes($options);
+
+        $value = e($this->formatLabel($name, $value));
+
+        return '<label for="'.$name.'"'.$options.'>'.$value;
+    }
+
+    public function endLabel()
+    {
+        return '</label>';
     }
 }
